ee45852ccd6829bdfc5b289e955310856f26dbc9
GauravM512 <gauravm5612@gmail.com>
2023-07-02 06:53:16 +0000

    refactor player , added auto play

--- cogs/music.py
+++ cogs/music.py
@@ -6,26 +6,23 @@ import wavelink
 from discord.ext import commands
 from discord.ext.commands import Context
 
+class CustomPlayer(wavelink.Player):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.autoplay = True
 
-async def link_validation(link: str):
-    playlist_regex = r"^(https?:\/\/)?(www\.)?(youtube\.com)\/(playlist\?|watch\?)(list=)([a-zA-Z0-9_-]+)"
-    video_regex = r"^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/(watch\?v=|embed\/|v\/)?([a-zA-Z0-9_-]+)"
+    
 
-    if re.match(playlist_regex, link):
-        # Check if the link contains the "list" parameter
-        if "list=" in link:
-            return "playlist"
-
-    elif re.match(video_regex, link):
-        return "video"
-
-    else:
-        return "string"
-
+async def check_author(ctx: Context):
+    if ctx.author.voice is None:
+        await ctx.reply("Bhai Voice chat join karega pehle", mention_author=False)
+        return False
+    return True
 
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.auto = False
 
     @commands.Cog.listener()
     async def on_wavelink_node_ready(self, node: wavelink.Node) -> None:
@@ -37,20 +34,18 @@ class Music(commands.Cog):
         self, payload : wavelink.TrackEventPayload
     ):
         player: wavelink.Player=payload.player
-        if not player.queue.is_empty:
-            track = player.queue.get()
-            await player.play(track)
         try:
             await self.bot.wait_for("wavelink_track_start", timeout=300)
         except asyncio.TimeoutError:
+            self.auto = False
             await player.disconnect()
+
 
 
     @commands.command(aliases=["p"])
     async def play(self, ctx: Context, *args):
         """Play a song from YouTube"""
         query = " ".join(args)
-        media_type = await link_validation(query)
 
         if ctx.author.voice is None:
             await ctx.reply("Bhai Voice chat join karega pehle", mention_author=False)
@@ -59,57 +54,49 @@ class Music(commands.Cog):
         if not query:
             await ctx.reply("Song toh likh bhai", mention_author=False)
             return
+        player:wavelink.Player
+        player = ctx.guild.voice_client or await ctx.author.voice.channel.connect(cls=wavelink.Player)
+        player.autoplay = True
 
-        if not ctx.guild.voice_client:
-            player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
-        else:
-            player = ctx.guild.voice_client
-        
         if player.channel.id != ctx.author.voice.channel.id:
-            if player.is_playing():
-                await ctx.send("Bhai tujhe dikh nai ra me kahi aur baja raha hu", mention_author=False)
-                return
-            else:
-                await player.disconnect()
-                player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+            await ctx.send("Bhai tujhe dikh nai ra me kahi aur baja raha hu", mention_author=False)
 
-        if ctx.guild.voice_client:
-            player = ctx.guild.voice_client
-            if player.is_playing():
-                if media_type == "playlist":
-                    return await ctx.reply("Playlist nai bajata me.", mention_author=False)
-                if media_type == "video":
-                    query = re.sub(r"list=[^&?]+", "", query)
-                track = await wavelink.YouTubeTrack.search(query)
-                player.queue.put(track[0])
-                embed = discord.Embed(title="Added to Queue", description=track[0].title, color=0x00ff00)
-                embed.set_image(url=track[0].thumbnail)
+        tracks = await wavelink.YouTubeTrack.search(query)
+        if isinstance(tracks, (list)):
+            if not player.is_playing():
+                await player.play(tracks[0],populate=self.auto)
+                embed = discord.Embed(title="Now Playing", description=f"[{tracks[0].title}]({tracks[0].uri})", color=discord.Color.blurple())
+                embed.set_image(url=tracks[0].thumb)
                 await ctx.reply(embed=embed, mention_author=False)
-                return
+            else:
+                await player.queue.put_wait(tracks[0])
+                await ctx.reply(f"Queued {tracks[0].title}", mention_author=False)
 
-        if media_type == "playlist":
-            await ctx.reply("Sorry bhai playlist nai bajata me", mention_author=False)
-            return
+        elif isinstance(tracks, wavelink.YouTubePlaylist):
+            await player.queue.put_wait(tracks)
+            if not player.is_playing():
+                track = player.queue.get()
+                await player.play(track,populate=self.auto)
+                embed = discord.Embed(title="Now Playing", description=f"[{track.title}]({track.uri})", color=discord.Color.blurple())
+                embed.set_image(url=track.thumb)
+                await ctx.reply(embed=embed, mention_author=False)
+            else:
+                await ctx.reply(f"Queued Playlist ", mention_author=False)
 
-        if media_type in ["string", "video"]:
-            query = re.sub(r"list=[^&?]+", "", query)
-            tracks = await wavelink.YouTubeTrack.search(query)
-            await player.play(tracks[0])
-            embed = discord.Embed(title="Now Playing", description=tracks[0].title, color=0x00ff00)
-            embed.set_image(url=tracks[0].thumbnail)
-            await ctx.reply(embed=embed)
-            return
+        else:
+            reply_message = "Kuch nai mila bhai"
+            await ctx.reply(reply_message, mention_author=False)
 
-        if not tracks:
-            await ctx.reply("Dhang se likh na lawde kuch result nai aya", mention_author=False)
-            return
 
-        
+
 
 
     @commands.command(aliases=["pa"])
     async def pause(self, ctx:Context):
         """Pause the current song"""
+        check = await check_author(ctx)
+        if not check:
+            return
         player: wavelink.Player = ctx.guild.voice_client
         if not player.is_playing():
             await ctx.reply("kya pause karu jab kuch nai baj raha", mention_author=False)
@@ -120,6 +107,9 @@ class Music(commands.Cog):
     @commands.command(aliases=["r"])
     async def resume(self, ctx:Context):
         """Resume the current song"""
+        check = await check_author(ctx)
+        if not check:
+            return
         player: wavelink.Player = ctx.guild.voice_client
         if player.is_playing():
             await ctx.reply("pehle se baj raha h bhai", mention_author=False)
@@ -133,20 +123,22 @@ class Music(commands.Cog):
     @commands.command(aliases=["s","dc"])
     async def stop(self, ctx:Context):
         """Stop the current song"""
+        check = await check_author(ctx)
+        if not check:
+            return
         player: wavelink.Player = ctx.guild.voice_client
         if player.queue.is_empty != True:
             player.queue.clear()
-        if player.is_playing():
-            await player.stop()
-            await player.disconnect()
-            await ctx.message.add_reaction("👍")
-            return
         await player.disconnect()
+        self.auto = False
         await ctx.message.add_reaction("👍")
 
     @commands.command(aliases=["sk","n","next"])
     async def skip(self, ctx:Context):
         """Skip the current song"""
+        check = await check_author(ctx)
+        if not check:
+            return
         player: wavelink.Player = ctx.guild.voice_client
         if not player.is_playing():
             await ctx.reply("kya skip karu jab kuch nai baj raha",mention_author=False)
@@ -157,6 +149,9 @@ class Music(commands.Cog):
     @commands.command(aliases=["q"])
     async def queue(self, ctx:Context):
         """Show the current queue"""
+        check = await check_author(ctx)
+        if not check:
+            return
         player: wavelink.Player = ctx.guild.voice_client
         if player.queue.is_empty:
             await ctx.reply("kya queue dikhau jab kuch queue khali hain")
@@ -169,6 +164,9 @@ class Music(commands.Cog):
     @commands.command()
     async def remove(self, ctx:Context, index:int):
         """Remove a song from queue"""
+        check = await check_author(ctx)
+        if not check:
+            return
         player: wavelink.Player = ctx.guild.voice_client
         if player.queue.is_empty:
             await ctx.reply("kya remove karu jab kuch queue khali hain")
@@ -182,24 +180,46 @@ class Music(commands.Cog):
     @commands.command(aliases=["np"])
     async def now_playing(self, ctx:Context):
         """Show the current song"""
+        check = await check_author(ctx)
+        if not check:
+            return
         player: wavelink.Player = ctx.guild.voice_client
         if not player.is_playing():
             await ctx.reply("kya dikhau jab kuch nai baj raha")
             return
         embed = discord.Embed(title="Now Playing", description=player.current.title, color=0x00ff00)
-        #make emoji duration
         embed.set_image(url=player.current.thumbnail)
         await ctx.reply(embed=embed, mention_author=False)
 
     @commands.command(aliases=["vol"])
-    async def volume(self, ctx:Context, volume:int):
+    async def volume(self, ctx:Context, volume:int | None):
         """Change the volume"""
+        check = await check_author(ctx)
+        if not check:
+            return
+        if volume is None:
+            await ctx.reply(f"Current volume {ctx.guild.voice_client.volume}",mention_author=False)
+            return
         player: wavelink.Player = ctx.guild.voice_client
         if volume > 100 or volume < 0:
             await ctx.reply("volume 0 se 100 ke beech me hona chahiye bhai")
             return
         await player.set_volume(volume)
         await ctx.reply(f"{volume} kardia volume",mention_author=False)
+
+    @commands.command(aliases=["auto"])
+    async def autoplay(self, ctx:Context):
+        """Toggle autoplay"""
+        check= await check_author(ctx)
+        if not check:
+            return
+        player:wavelink.Player = ctx.guild.voice_client or await ctx.author.voice.channel.connect(cls=wavelink.Player)
+        self.auto = not self.auto
+        if self.auto:
+            await ctx.reply("autoplay on kardia bhai",mention_author=False)
+        else:
+            await ctx.reply("autoplay off kardia bhai",mention_author=False)
+
 
 
 
